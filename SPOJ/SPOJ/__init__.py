b178ba4fb2bd391d5d54b8a5e51fe3e44bbdba7e
jnguyen1192 <johnny.nguyen@etu.unice.fr>
2018-03-05 16:26:52 +0100

    #12 FCTRL solved closes #12

--- SPOJ/SPOJ/__init__.py
+++ SPOJ/SPOJ/__init__.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python
-from SPOJ.fctrl import *
+from sys import stdin, stdout
+
+
+def analyse_fctrl(expression):
+    # @source http://www.purplemath.com/modules/factzero.htm
+    if expression < 4:
+        return "0"
+    calc = 2
+    exp = 1
+    nums = []
+    while calc >= 1:
+        calc = expression / pow(5, exp)
+        if calc >= 1:
+            nums.append(calc)
+        exp += 1
+    return str(sum(nums))
+
+
+def fctrl(nb_line):
+    res = ""
+    listres = []
+    while nb_line != 0:
+        expression = stdin.readline()
+        listres.append(analyse_fctrl(int(expression.replace("\n", ""))) + '\n')
+        nb_line -= 1
+    res = res.join(listres)
+    stdout.write(res[:-1])
+
 
 
 def main():
