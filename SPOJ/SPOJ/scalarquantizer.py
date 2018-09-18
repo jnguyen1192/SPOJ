eaeebd2ffe659cd6e490551bda4f0bacb7934760
jnguyen1192 <johnny.nguyen@etu.unice.fr>
2018-09-18 15:33:48 +0200

    #14 Distorsion plot created

--- SPOJ/SPOJ/scalarquantizer.py
+++ SPOJ/SPOJ/scalarquantizer.py
@@ -4,11 +4,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 
-R = 1
-L = 2 ** R
-
-
-def delta(Smax, Smin):
+def delta(Smax, Smin, R):
+    L = 2 ** R
     return (Smax-Smin)/L
 
 
@@ -21,16 +18,20 @@ def scalar_quantizer(Signal, Smin, D):
     return S
 
 
-def plot(Sign, x):
-    Signal = Sign(x)
+def imgsave(i, l, h, SignalFin):
+    # create a new img
+    res = Image.new("L", (l, h))
+    # fill the img
+    res.putdata(SignalFin)
+    # save the file with his name
+    res.save("new"+i)
+
+
+def quantizer_algorithm(Signal, R):
     Max = np.max(Signal)
     Min = np.min(Signal)
-    Delta = delta(Max, Min)
-
-    SignalFin = scalar_quantizer(Signal, Min, Delta)
-    plt.plot(Signal)
-    plt.plot(SignalFin)
-    plt.show()
+    Delta = delta(Max, Min, R)
+    return scalar_quantizer(Signal, Min, Delta)
 
 
 def plot_img(i):
@@ -42,23 +43,46 @@ def plot_img(i):
     l, h = image.size
 
     # Quantizer algorithm
+    SignalFin = quantizer_algorithm(Signal, 1)
+
+    imgsave(i, l, h, SignalFin)
+'''
+def plot(Sign, x):
+    Signal = Sign(x)
     Max = np.max(Signal)
     Min = np.min(Signal)
     Delta = delta(Max, Min)
+
     SignalFin = scalar_quantizer(Signal, Min, Delta)
+    plt.plot(Signal)
+    plt.plot(SignalFin)
+    plt.show()
+'''
 
-    # create a new img
-    res = Image.new("L", (l,h))
-    # fill the img
-    res.putdata(SignalFin)
-    # save the file with his name
-    res.save("new"+i)
 
+def D(R):
+    image = Image.open('lena512.bmp')
+    Signal = image.getdata()
+    N = len(Signal)
+    print('N ', N)
+    SignalFin = quantizer_algorithm(Signal, R)
+
+    return (1.0/N) * sum([abs((Signal[n] - SignalFin[n])) ** 2 for n in range(1, N)])
+
+
+def distorsion():
+    return [D(i) for i in range(1, 8)]
+
+
+def plot_distorsion():
+    plt.plot(distorsion())
+    plt.show()
 
 def main():
     #test sin
     #plot(np.sin, np.linspace(-np.pi, np.pi, 201))
-    plot_img('lena512.bmp')
+    #plot_img('lena512.bmp')
+    plot_distorsion()
 
 main()
 
